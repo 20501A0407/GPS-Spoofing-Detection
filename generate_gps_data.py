ab8da07393149bed843133148de3ed8b408090e2
20501A0407 <105499763+20501A0407@users.noreply.github.com>
2025-04-07 08:43:52 +0530

    Update generate_gps_data.py

--- generate_gps_data.py
+++ generate_gps_data.py
@@ -2,7 +2,7 @@ import random
 import sqlite3
 from datetime import datetime
 
-# Function to log GPS data into SQLite database
+# Function to log gos data to sqlite
 def log_event_db(gps_signal, is_authentic):
     conn = sqlite3.connect("database.db")
     cursor = conn.cursor()
@@ -44,4 +44,4 @@ for _ in range(100):  # 10 to 100 samples
     log_event_db(generate_random_gps_data(is_spoofed=False), True)
     log_event_db(generate_random_gps_data(is_spoofed=True), False)
 
-print("✅ GPS Data Logged Successfully! Run `train_ml_model.py` to train the model.")
+print(" GPS Data Logged Successfully! Run `train_ml_model.py` to train the model.")
